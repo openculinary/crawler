52d6859d197207a53aa3fe2a2071dc11818d5d72
James Addison <james@reciperadar.com>
2019-10-31 07:18:34 -0700

    Add test coverage

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,2 +1,11 @@
+from web.app import get_domain
+
+
 def test_request(client):
     client.post('/')
+
+
+def test_get_domain():
+    domain = get_domain('https://recipe.subdomain.example.com/recipe/123')
+
+    assert domain == 'example.com'
