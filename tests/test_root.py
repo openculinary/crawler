2896ca42214de55171af4ae18aa61a83f992abb3
James Addison <james@reciperadar.com>
2019-12-22 12:14:27 +0000

    Update test coverage

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,11 +1,32 @@
+import pytest
+import responses
+
 from web.app import get_domain
 
 
-def test_request(client):
-    client.post('/')
+@pytest.fixture
+def origin_url():
+    return 'https://recipe.subdomain.example.com/recipe/123'
 
 
-def test_get_domain():
-    domain = get_domain('https://recipe.subdomain.example.com/recipe/123')
+@pytest.fixture
+def content_url(origin_url):
+    return origin_url.replace('subdomain', 'migrated')
+
+
+def test_get_domain(origin_url):
+    domain = get_domain(origin_url)
 
     assert domain == 'example.com'
+
+
+@responses.activate
+def test_origin_url_resolution(client, origin_url, content_url):
+    redir_headers = {'Location': content_url}
+    responses.add(responses.GET, origin_url, status=301, headers=redir_headers)
+    responses.add(responses.GET, content_url, status=200)
+
+    response = client.post('/resolve', data={'url': origin_url})
+    recipe_url = response.json['resolves_to']
+
+    assert recipe_url == content_url
