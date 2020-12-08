1be6dd82510cdeeec25a4f0baf1a24461061871e
James Addison <james@reciperadar.com>
2020-12-08 12:31:42 +0000

    Update to recipe-scrapers==10.1.1

--- tests/test_root.py
+++ tests/test_root.py
@@ -99,4 +99,4 @@ def test_crawl_response(image_version, scrape_recipe, parse_directions,
 
     assert response.status_code == 200
     assert service_version == 'test_version'
-    assert rs_version == '10.1.0'
+    assert rs_version == '10.1.1'
