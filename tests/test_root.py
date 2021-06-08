c7bed728798f36397673d5829540ee240bc1a51d
James Addison <james@reciperadar.com>
2021-06-08 22:18:22 +0100

    Update dependencies

--- tests/test_root.py
+++ tests/test_root.py
@@ -133,7 +133,7 @@ def test_crawl_response(
 
     assert response.status_code == 200
     assert service_version == 'test_version'
-    assert rs_version == '13.2.3'
+    assert rs_version == '13.2.7'
 
     nutrition = response.json.get('recipe', {}).get('nutrition')
     assert nutrition is not None
