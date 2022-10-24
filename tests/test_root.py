9c21029c6a98c19a3af12e07d952aa24d8ae74f1
James Addison <james@reciperadar.com>
2022-10-24 23:15:27 +0100

    Update to recipe-scrapers v14.21.0

--- tests/test_root.py
+++ tests/test_root.py
@@ -135,7 +135,7 @@ def test_crawl_response(
 
     assert response.status_code == 200
     assert service_version == "test_version"
-    assert rs_version == "14.20.0"
+    assert rs_version == "14.21.0"
 
     nutrition = response.json.get("recipe", {}).get("nutrition")
     assert nutrition is not None
