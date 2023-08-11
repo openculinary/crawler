797d385f77a2621569af60b8681829118e6fc8b7
James Addison <james@reciperadar.com>
2023-08-11 18:14:12 +0100

    RecipeRadar datasource: adjust logic to submit the source (origin) URL instead of the destination URL to the backend service

--- reciperadar/recipes.py
+++ reciperadar/recipes.py
@@ -8,7 +8,7 @@ from actions import recrawl, reindex
 def query_recipes(where):
     where = where or "true"
     query = (
-        f"select id, dst, title "
+        f"select id, src, title "
         f"from recipes "
         f"where {where} "
         f"order by random()"
@@ -17,8 +17,8 @@ def query_recipes(where):
     db = pg8000.connect(host="192.168.100.1", user="api", database="api")
     cursor = db.cursor()
     results = cursor.execute(query)
-    for recipe_id, dst, title in results.fetchall():
-        yield recipe_id, dst, title
+    for recipe_id, src, title in results.fetchall():
+        yield recipe_id, src, title
     cursor.close()
 
 
@@ -28,9 +28,9 @@ parser.add_argument("--recrawl", action="store_true", help="Invoke recrawling")
 parser.add_argument("--reindex", action="store_true", help="Invoke reindexing")
 args = parser.parse_args()
 
-for recipe_id, dst, title in query_recipes(args.where):
+for recipe_id, src, title in query_recipes(args.where):
     if args.recrawl:
-        recrawl(dst)
+        recrawl(src)
         print(f"* Processed recipe {title} for recrawling")
     elif args.reindex:
         reindex(recipe_id)
