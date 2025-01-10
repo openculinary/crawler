8741b0bed14dda29a02f246ecfc16a6b2599e3cc
James Addison <55152140+jayaddison@users.noreply.github.com>
2025-01-10 19:07:56 +0000

    Add logic to respect the robots.txt 'Crawl-Delay' directive (#41)

--- web/robots.py
+++ web/robots.py
@@ -3,7 +3,7 @@ from urllib.parse import urljoin
 
 from cacheout import Cache
 import requests
-from robotexclusionrulesparser import RobotExclusionRulesParser
+from robotexclusionrulesparser import RobotExclusionRulesParser, _Ruleset
 
 from web.domains import get_domain
 from web.web_clients import HEADERS_DEFAULT
@@ -12,6 +12,17 @@ web_client = requests.Session()
 
 
 domain_robot_parsers = Cache(ttl=60 * 60, timer=time)  # 1hr cache expiry
+
+
+# Workaround: recognize robots.txt rulesets containining solely a crawl-delay.
+class _PatchedRuleset(_Ruleset):
+    _is_not_empty = _Ruleset.is_not_empty
+
+    def is_not_empty(self):
+        return _PatchedRuleset._is_not_empty(self) or self.crawl_delay is not None
+
+
+_Ruleset.is_not_empty = _PatchedRuleset.is_not_empty
 
 
 def get_robot_parser(url):
@@ -28,3 +39,9 @@ def can_fetch(url):
     robot_parser = get_robot_parser(url)
     user_agent = HEADERS_DEFAULT.get("User-Agent", "*")
     return robot_parser.is_allowed(user_agent, url)
+
+
+def crawl_delay(url):
+    robot_parser = get_robot_parser(url)
+    user_agent = HEADERS_DEFAULT.get("User-Agent", "*")
+    return max(1, robot_parser.get_crawl_delay(user_agent) or 0)
